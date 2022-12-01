90df41fcd26f6a880a19a0ccf64fddfefc6112ee
HenryGibbens <79278548+HenryGibbens@users.noreply.github.com>
2022-12-01 10:32:44 +0000

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,29 @@
+enum RadioMessage {
+    message1 = 49434,
+    left = 14947,
+    right = 32391,
+    backwards = 56537,
+    forward = 16348
+}
+input.onButtonPressed(Button.A, function () {
+    radio.sendMessage(RadioMessage.left)
+})
+input.onGesture(Gesture.LogoUp, function () {
+	
+})
+input.onGesture(Gesture.TiltLeft, function () {
+	
+})
+input.onGesture(Gesture.ScreenUp, function () {
+    radio.sendMessage(RadioMessage.forward)
+})
+input.onButtonPressed(Button.AB, function () {
+    radio.sendMessage(RadioMessage.backwards)
+})
+input.onButtonPressed(Button.B, function () {
+    radio.sendMessage(RadioMessage.right)
+})
+input.onGesture(Gesture.TiltRight, function () {
+	
+})
+radio.setGroup(99)
